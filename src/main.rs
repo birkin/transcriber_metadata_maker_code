cb74bd50bfc95e3e3dbea32bb63fa01d9b48f8a7
Birkin James Diana <birkin.diana@gmail.com>
2022-12-26 22:34:30 -0500

    implements initial get.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::*;
+use serde_json;
+use tokio::*;
+
+#[tokio::main]
+async fn main() {
+    // let search_url: String = "https://repository.library.brown.edu/api/search/?q=&selected_facets=mods_type_of_resource%3Amoving+image".to_string();
+    let search_url: String = "http://httpbin.org/json".to_string();
+
+    let resp = reqwest::get(search_url).await.expect("REASON").text().await;
+    println!("body = {:?}", resp);
 }
+
+// http://httpbin.org/json

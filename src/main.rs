fedaf2bfab29defb8ace708596cd529fa094e045
Birkin James Diana <birkin.diana@gmail.com>
2022-12-27 12:24:13 -0500

    refactors search-url query into called function.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::arch::aarch64::int32x2_t;
+
 use reqwest::*;
 use serde_json;
 use tokio::*;
@@ -6,9 +8,32 @@ use tokio::*;
 async fn main() {
     // let search_url: String = "https://repository.library.brown.edu/api/search/?q=&selected_facets=mods_type_of_resource%3Amoving+image".to_string();
     let search_url: String = "http://httpbin.org/json".to_string();
+    println!("search_url, ``{}``", search_url);
 
-    let resp = reqwest::get(search_url).await.expect("REASON").text().await;
-    println!("body = {:?}", resp);
+    // let resp = reqwest::get(search_url).await.expect("REASON").text().await;
+    let search_json: String = get_search_json(search_url).await;
+    // println!("body = {:?}", search_json);
 }
+
+async fn get_search_json(search_url: String) -> String {
+    println!("starting get_search_json()");
+
+    // query search-url -------------------------
+    let resp = reqwest::get(search_url).await.unwrap_or_else(|error| {
+        panic!("Problem accessing search-url: ``{:?}``", error);
+    });
+    println!("resp, ``{:?}``", resp);
+
+    // get text from response -------------------
+    let search_json: String = resp.text().await.unwrap_or_else(|error| {
+        panic!("Problem extracting text from response: ``{:?}``", error);
+    });
+    println!("search_json, ``{:?}``", search_json);
+
+    // return -----------------------------------
+    search_json
+}
+
+// misc stuff -------------------------------------------------------
 
 // http://httpbin.org/json
